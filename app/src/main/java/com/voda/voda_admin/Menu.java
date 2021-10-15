5c5791f7afe542b3fa4ea22f53422e53603982fe
SeJin <tpwls337@naver.com>
2021-10-15 23:44:09 +0900

    메뉴 등록 프로토타입 complete

---
+++ app/src/main/java/com/voda/voda_admin/Menu.java
@@ -0,0 +1,41 @@
+package com.voda.voda_admin;
+
+public class Menu {
+    String name,text,text_addition,category;
+
+    public Menu(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText_addition() {
+        return text_addition;
+    }
+
+    public void setText_addition(String text_addition) {
+        this.text_addition = text_addition;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
--- app/src/main/java/com/voda/voda_admin/MenuActivity.java
+++ app/src/main/java/com/voda/voda_admin/MenuActivity.java
@@ -1,14 +1,98 @@
 package com.voda.voda_admin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    Button btn_menu_plus;
+    String mycategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btn_menu_plus = findViewById(R.id.btn_menu_plus);
+        btn_menu_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu_dialog(v);
+            }
+        });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("voda_handy");
+    }
+
+    public void menu_dialog(View v) {
+        EditText edit_menu_name, edit_menu_category, edit_menu_text, edit_menu_text_addition;
+        Button btn_menu_register;
+
+        View dialogView = getLayoutInflater().inflate(R.layout.menuplusdialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setView(dialogView);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.white);
+        alertDialog.show();
+
+        edit_menu_name = dialogView.findViewById(R.id.edit_menu_name);
+        edit_menu_category = dialogView.findViewById(R.id.edit_menu_category);
+        edit_menu_text = dialogView.findViewById(R.id.edit_menu_text);
+        edit_menu_text_addition = dialogView.findViewById(R.id.edit_menu_text_addition);
+        btn_menu_register = dialogView.findViewById(R.id.btn_menu_register);
+
+        btn_menu_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                String name = edit_menu_name.getText().toString();
+                String category = edit_menu_category.getText().toString();
+                String text = edit_menu_text.getText().toString();
+                String text_addition = edit_menu_text_addition.getText().toString();
+
+                Menu menu = new Menu();
+                menu.setName(name);
+                menu.setCategory(category);
+                menu.setText(text);
+                menu.setText_addition(text_addition);
+
+                FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                mDatabaseRef.child("UserAccount").child("admin").child(firebaseUser.getUid()).child("category").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                        Log.d("아ss녕","ㄹㅇㄹ");
+                        mycategory = (String) datasnapshot.getValue();
+                        mDatabaseRef.child("Store").child(mycategory).child(firebaseUser.getUid()).child("MenuInfo").child(name).setValue(menu);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.d("아녕","ㄹㅇㄹ");
+                    }
+                });
+            }
+        });
     }
 }
