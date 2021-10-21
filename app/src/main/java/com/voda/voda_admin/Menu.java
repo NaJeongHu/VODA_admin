45f72d607c9f8f5e502e4eb72b8f41dedc98e461
NaJeongHu <kise912@knu.ac.kr>
2021-10-21 17:35:04 +0900

    dd

--- app/src/main/java/com/voda/voda_admin/Menu.java
+++ app/src/main/java/com/voda/voda_admin/Menu.java
@@ -1,41 +1,32 @@
 package com.voda.voda_admin;
 
 public class Menu {
-    String name,text,text_addition,category;
 
-    public Menu(){
+    private String name;
+    private String category;
+    private String explanation;
+    private String tag;
+    private Integer price;
 
-    }
+    public Menu(){ }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getText() {
-        return text;
-    }
+    public String getCategory() { return category; }
 
-    public void setText(String text) {
-        this.text = text;
-    }
+    public void setCategory(String category) { this.category = category; }
 
-    public String getText_addition() {
-        return text_addition;
-    }
+    public String getExplanation() { return explanation; }
 
-    public void setText_addition(String text_addition) {
-        this.text_addition = text_addition;
-    }
+    public void setExplanation(String explanation) { this.explanation = explanation; }
 
-    public String getCategory() {
-        return category;
-    }
+    public String getTag() { return tag; }
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
+    public void setTag(String tag) { this.tag = tag; }
+
+    public Integer getPrice() { return price; }
+
+    public void setPrice(Integer price) { this.price = price; }
 }
